b8506307b1e401c36ce6de7b5ba86627c6450ad8
Shatbera <81585007+Shatbera@users.noreply.github.com>
2025-04-21 00:25:01 +0400

    #refactor drag and drop

--- LearningPlatform_UP/Assets/Project/Scripts/UI/Lab/LabSampleItem.cs
+++ LearningPlatform_UP/Assets/Project/Scripts/UI/Lab/LabSampleItem.cs
@@ -8,18 +8,11 @@ public class LabSampleItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     [SerializeField] private RectTransform _dragContainer;
 
     private CollectableItemEntry<ResearchSampleSO> _sampleEntry;
-    private RectTransform _canvasRectTransform;
-    private RectTransform _dragRect;
-    private Vector2 _offset;
-    private Transform _originalParent;
-    private Vector2 _originalPosition;
     private Canvas _canvas;
 
     private void Awake()
     {
-        _dragRect = _dragContainer;
         _canvas = GetComponentInParent<Canvas>();
-        _canvasRectTransform = _canvas.GetComponent<RectTransform>();
     }
 
     public void Setup(CollectableItemEntry<ResearchSampleSO> entry)
@@ -35,29 +28,17 @@ public class LabSampleItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        _originalParent = _dragRect.parent;
-        _originalPosition = _dragRect.anchoredPosition;
-
-        _dragRect.SetParent(_canvas.transform);
-
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            _canvasRectTransform, eventData.position, _canvas.worldCamera, out Vector2 localPointerPosition);
-
-        _offset = _dragRect.anchoredPosition - localPointerPosition;
+        _dragContainer.SetParent(_canvas.transform);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            _canvasRectTransform, eventData.position, _canvas.worldCamera, out Vector2 localPointerPosition))
-        {
-            _dragRect.anchoredPosition = localPointerPosition + _offset;
-        }
+        _dragContainer.anchoredPosition += eventData.delta;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        _dragRect.SetParent(_originalParent);
-        _dragRect.anchoredPosition = _originalPosition;
+        _dragContainer.SetParent(transform);
+        _dragContainer.localPosition = Vector2.zero;
     }
 }
